1f6a769f10ce8859b47b462cfe32887c59aa9814
Tatiana Kashcha <tkashcha@gmail.com>
2024-01-04 00:49:56 +0200

    update

--- examples/nextjs-server-actions-upload/src/app/page.tsx
+++ examples/nextjs-server-actions-upload/src/app/page.tsx
@@ -1,17 +1,17 @@
-import { revalidatePath } from 'next/cache';
-import { v2 as cloudinary } from 'cloudinary';
+import { revalidatePath } from "next/cache";
+import { v2 as cloudinary } from "cloudinary";
 
-import Container from '@/components/Container';
-import CldImage from '@/components/CldImage';
-import Button from '@/components/Button';
+import Container from "@/components/Container";
+import CldImage from "@/components/CldImage";
+import Button from "@/components/Button";
 
-import images from '@/data/images.json';
+import images from "@/data/images.json";
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+});
 
 interface CloudinaryResource {
   context?: {
@@ -23,31 +23,41 @@ interface CloudinaryResource {
 }
 
 async function Home() {
-  const { resources: sneakers } = await cloudinary.api.resources_by_tag('nextjs-server-actions-upload-sneakers', { context: true });
+  const { resources: sneakers } = await cloudinary.api.resources_by_tag(
+    "nextjs-server-actions-upload-sneakers",
+    { context: true }
+  );
 
   async function create(formData: FormData) {
-    'use server'
-    const file = formData.get('image') as File;
+    "use server";
+    const file = formData.get("image") as File;
     const arrayBuffer = await file.arrayBuffer();
     const buffer = new Uint8Array(arrayBuffer);
     await new Promise((resolve, reject) => {
-      cloudinary.uploader.upload_stream({
-        tags: ['nextjs-server-actions-upload-sneakers']
-      }, function (error, result) {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve(result);
-      })
-      .end(buffer);
+      cloudinary.uploader
+        .upload_stream(
+          {
+            tags: ["nextjs-server-actions-upload-sneakers"],
+          },
+          function (error, result) {
+            if (error) {
+              reject(error);
+              return;
+            }
+            resolve(result);
+          }
+        )
+        .end(buffer);
     });
-    revalidatePath('/')
+    revalidatePath("/");
   }
   return (
     <Container>
       <h2 className="text-xl font-bold mb-4">Add a New Image</h2>
-      <form action={create} className="bg-white border border-slate-200 dark:border-slate-500 rounded p-6 mb-6">
+      <form
+        action={create}
+        className="bg-white border border-slate-200 dark:border-slate-500 rounded p-6 mb-6"
+      >
         <p className="mb-6">
           <label htmlFor="image" className="block font-semibold text-sm mb-2">
             Select an Image to Upload
@@ -66,33 +76,38 @@ async function Home() {
       <ul className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-12">
         {sneakers.map((sneaker: CloudinaryResource) => {
           return (
-            <li key={sneaker.public_id} className="rounded overflow-hidden bg-white dark:bg-slate-700">
+            <li
+              key={sneaker.public_id}
+              className="rounded overflow-hidden bg-white dark:bg-slate-700"
+            >
               <div className="relative">
                 <CldImage
                   width={800}
                   height={600}
                   src={sneaker.public_id}
-                  alt={sneaker.context?.alt || ''}
+                  alt={sneaker.context?.alt || ""}
                 />
               </div>
-              { sneaker.context?.caption && (
+              {sneaker.context?.caption && (
                 <div className="py-4 px-5">
                   <p className="mb-1 text-md font-bold leading-tight text-neutral-800 dark:text-neutral-50">
-                    { sneaker.context?.caption || '' }
+                    {sneaker.context?.caption || ""}
                   </p>
                 </div>
               )}
-              
             </li>
-          )
+          );
         })}
       </ul>
       <h2 className="text-xl font-bold mb-4">Links</h2>
       <ul>
-        {images.map(image => {
+        {images.map((image) => {
           return (
             <li key={image.name}>
-              { image.name }: <a className="text-blue-600" href={image.link}>{ image.link }</a>
+              {image.name}:{" "}
+              <a className="text-blue-600" href={image.link}>
+                {image.link}
+              </a>
             </li>
           );
         })}
@@ -100,11 +115,18 @@ async function Home() {
 
       <div className="mt-12 py-10 border-[0] border-t border-solid border-slate-300">
         <p>
-          Find more Cloudinary examples at: <a className="underline text-inherit" href="https://github.com/cloudinary-community/cloudinary-examples">github.com/cloudinary-community/cloudinary-examples</a>.
+          Find more Cloudinary examples at:{" "}
+          <a
+            className="underline text-inherit"
+            href="https://github.com/cloudinary-community/cloudinary-examples"
+          >
+            github.com/cloudinary-community/cloudinary-examples
+          </a>
+          .
         </p>
       </div>
     </Container>
-  )
+  );
 }
 
 export default Home;
